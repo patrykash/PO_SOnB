0152f4a74f09b16457183489f558bc1095738b90
patrykash <patryksieczka4@gmail.com>
2021-10-23 18:17:10 +0200

    Add clientHandler

---
+++ src/main/java/com/example/sonb/model/ClientHandler.java
@@ -0,0 +1,39 @@
+package com.example.sonb.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler extends Thread {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket socket) {
+        clientSocket = socket;
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void read() {
+        try {
+            String inputLine = in.readLine();
+            System.out.println("Wiadomosc dostarczona na serwer : " + inputLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void send(int clientId) {
+        out.println("Wiadomosc z serwera dla klienta : " + clientId);
+    }
+
+}
+
