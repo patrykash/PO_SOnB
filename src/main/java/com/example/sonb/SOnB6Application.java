fc53aefbd3b986612fa6b0d78d14711650a15f7f
patrykash <patryksieczka4@gmail.com>
2021-10-23 16:39:00 +0200

    Initial commit

---
+++ src/main/java/com/example/sonb/SOnB6Application.java
@@ -0,0 +1,13 @@
+package com.example.sonb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SOnB6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SOnB6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sonb/SOnB6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SOnB6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
