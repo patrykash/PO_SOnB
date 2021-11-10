4859304c17be7823b38dfa0187290fe3d364c25e
patrykash <patryksieczka4@gmail.com>
2021-11-10 18:15:10 +0100

    Add constructor to SimpleServer

--- src/main/java/com/example/sonb/model/SimpleServer.java
+++ src/main/java/com/example/sonb/model/SimpleServer.java
@@ -3,6 +3,10 @@ package com.example.sonb.model;
 public class SimpleServer extends MainServer {
     private Client client;
 
+    public SimpleServer(int port) {
+        super(port);
+    }
+
     public void createClient() {
         client = new Client();
     }
