21daa0c341cfe1a393b2cf20dff3d65014092d30
patrykash <patryksieczka4@gmail.com>
2022-01-06 22:00:33 +0100

    Update view scripts

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,15 @@
+
 function init(){
 
     document.getElementById("startServer").addEventListener("click",startServer);
-    document.getElementById("startClients").addEventListener("click",startClients)
+    document.getElementById("startClients").addEventListener("click",runClients)
     document.getElementById("sendMessage").addEventListener("click", getMessage)
+    document.getElementById("serverError").addEventListener("click", runErrorWithServer)
+    document.getElementById("codeError").addEventListener("click", runErrorWithCoding)
+    document.getElementById("clientError").addEventListener("click", runErrorWithClient)
+    document.getElementById("serverFix").addEventListener("click", fixErrorWithServer)
+    document.getElementById("clientFix").addEventListener("click", fixErrorWithClient)
+    document.getElementById("codeFix").addEventListener("click", fixErrorWithCoding)
     /*window.sessionStorage.setItem("userId", "1");
     window.addEventListener("resize", setHeight);*/
 }
--- src/main/resources/static/js/service.js
+++ src/main/resources/static/js/service.js
@@ -1,30 +1,59 @@
 'use strict';
 window.onload = init;
-
+let serverId=0;
+let serverError = false;
 
 function startServer() {
-        let getQuestion = new XMLHttpRequest();
-        getQuestion.open('GET', '/server/start', true);
-        getQuestion.send(null);
-        getQuestion.onload = function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/server/start', true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
             console.log("dziala")
-        };
+        }
+    };
+}
+
+function runClients(){
+    console.log(serverError)
+    console.log(serverId)
+    if (serverError){
+        reconnectClients(serverId)
+    } else {
+        startClients()
+    }
 }
 
 function startClients() {
-    let getQuestion = new XMLHttpRequest();
-    getQuestion.open('GET', '/client/start', true);
-    getQuestion.send(null);
-    getQuestion.onload = function () {
-        console.log("dziala")
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/client/start', true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            setStatusImages(true)
+            console.log("dziala")
+        }
     };
 }
+
+function reconnectClients(clientId) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/client/connect/' + clientId, true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            setStatusImages(true)
+            console.log("dziala")
+        }
+    };
+}
+
 
 function sendMessage(message) {
     let send = new XMLHttpRequest();
     send.open('POST', '/server/send', true);
-    send.setRequestHeader("Content-Type", "application/json");
-    send.send(JSON.stringify(message));
+    send.setRequestHeader("Content-Type", "text/plain");
+    send.send(message);
     //send.send(message);
     send.onload = function () {
         console.log("wysylanie")
@@ -50,12 +79,113 @@ function getAnswers() {
 
 function setAnswers(answers){
     for (let i = 0; i < answers.length; i++) {
-        document.getElementById("client-message-"+ i).textContent = answers[i]
+        document.getElementById("client-message-"+ i).textContent = answers[i].message
+        document.getElementById("code-message-"+ i).textContent = answers[i].bergerCode
+        console.log(answers[i].correct)
+        if (answers[i].correct){
+            document.getElementById("client-correction-"+ i).textContent = 'Poprawna wiadomość'
+        } else{
+            document.getElementById("client-correction-"+ i).textContent = 'Blędna wiadomość'
+        }
     }
 }
 function getMessage() {
-    let message = document.getElementById("serverMessage").textContent;
+    let message = document.getElementById("serverMessage").value.toString();
+    console.log("wiadomosc z inputa : " + message);
+    console.log("wiadomosc z inputa : " + message.length);
     sendMessage(message);
 }
 
+function runErrorWithServer() {
+    serverId=1;
+    serverError = true;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/error/server', true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            console.log("/error/server")
+        }
+    };
+}
 
+function runErrorWithCoding() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/error/code', true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            console.log("/error/code")
+        }
+    };
+}
+
+function runErrorWithClient() {
+    let clientId = 1
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/error/client/'+clientId, true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            setStatusImage(false, clientId)
+            console.log('/error/client/'+clientId)
+        }
+    };
+}
+
+function fixErrorWithServer() {
+    serverId =1;
+    let clientId = 1
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/error/server/fix/'+clientId, true);
+    xhr.send(null)
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            console.log('/error/server/fix/'+clientId)
+        }
+    };
+}
+
+function fixErrorWithCoding() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/error/code/fix', true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            console.log('/error/code/fix')
+        }
+    };
+}
+
+function fixErrorWithClient() {
+    let clientId = 1
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/error/client/fix/'+clientId, true);
+    xhr.send(null);
+    xhr.onload = function () {
+        if (xhr.status === 200){
+            setStatusImage(true, clientId)
+            console.log('/client/fix/'+clientId)
+        }
+    };
+}
+
+function setStatusImages(isOn){
+    let images =  document.getElementsByClassName("status-img")
+    for (let image of images) {
+        if (isOn){
+            image.src = '../img/online.svg'
+        } else {
+            image.src = '../img/offline.svg'
+        }
+
+    }
+}
+function setStatusImage(isOn, clientId){
+    let images =  document.getElementsByClassName("status-img")
+    if (isOn){
+        images[clientId].src = '../img/online.svg'
+    } else {
+        images[clientId].src = '../img/offline.svg'
+    }
+}
